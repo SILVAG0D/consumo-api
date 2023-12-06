5b494f49c605ef068d524697ea06e55d9c1616cb
SILVAG0D <liesch576@gmail.com>
2023-12-05 22:30:30 -0300

    aprendendo a desserializacao e serializacao de modelos

--- lib/models/telefone_model.dart
+++ lib/models/telefone_model.dart
@@ -1,6 +1,36 @@
+import 'dart:convert';
+
 class TelefoneModel {
   int ddd;
   String numeroTelefone;
 
   TelefoneModel({required this.ddd, required this.numeroTelefone});
+
+  //! metodo que pega o nosso objeto telefone e transforma em um objeto Map<String,dynamic>
+  //toMap
+  //Passo 1
+  Map<String, dynamic> toMap(){
+    return {
+      'ddd': ddd,
+      'telefone': numeroTelefone
+    };
+  }
+
+  //! metodo que pega um Map<String,dynamic> e transforma em um json pelo pacote dart:convert (jsonEncode)
+  // toJson
+  //Passo 2
+  String toJson() => jsonEncode(toMap());
+
+
+  //Passo 2 da deserialização
+  factory TelefoneModel.fromMap(Map<String, dynamic> map) {
+    return TelefoneModel(ddd: map['ddd'], numeroTelefone: map['numeroTelefone']);
+  }
+
+
+  //Passo 1 da deserialização
+  factory TelefoneModel.fromJson(String json) {
+    final jsonMap = jsonDecode(json);
+    return TelefoneModel.fromMap(jsonMap);
+  }
 }
